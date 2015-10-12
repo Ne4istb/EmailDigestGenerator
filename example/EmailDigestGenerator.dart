c75153143576fdd031ce861696e9b3ae015b02d8
ne4istb <anechytailov@gmail.com>
2015-10-13 00:34:12 +0300

    Fixes #1. Added links clean up (untag and archive) after processing. Small refactoring.

--- example/EmailDigestGenerator.dart
+++ example/EmailDigestGenerator.dart
@@ -7,54 +7,26 @@ library EmailDigestGenerator.example;
 import 'package:EmailDigestGenerator/EmailDigestGenerator.dart';
 import 'package:pocket_client/pocket_client.dart';
 
-
 const consumerKey = '<consumer_key>';
-const accessToken = '<access_token>';
+const accessToken = '<access_code>';
+
+Client client = new Client(consumerKey, accessToken);
 
 main() {
-	var client = new Client(consumerKey, accessToken);
-
 	var options = new RetrieveOptions()
 		..detailType = DetailType.complete
 		..tag = 'digest';
 
-	client.getData(options: options).then(onDataReceived);
+	client.getData(options: options)
+	.then((PocketResponse response) => response.items.values)
+	.then(group)
+	.then(reorder)
+	.then(sendEmail);
+//	.then(cleanUp);
 }
 
-onDataReceived(PocketResponse response) {
-	Map<String, List<Link>> links = getLinks(response.items.values);
-
-	var recipients = ['anechytailov@gmail.com'];
-	var title = 'Digest title';
-
-	var template =
-	'<h1>$title</h1>'
-	'<div>'
-	'<!-- start group repeat -->'
-	'<h2>{{Group}}:</h2>'
-	'<!-- start repeat -->'
-	'<div class="section">'
-	//	    '<img src={{ImageUrl}} style />'
-	'<div>'
-	'<a href=\'{{Url}}\'>{{Title}}</a>'
-	'<div>{{Description}}</div>'
-	'</div>'
-	'</div>'
-	'<!-- end repeat -->'
-	'<!-- end group repeat -->'
-	'</div>';
-
-	var generator = new MailGenerator(template, links);
-	var result = generator.generate();
-
-	var mailer = new Mailer();
-
-	mailer.send(title, result, recipients);
-}
-
-Map<String, List<Link>> getLinks(items) {
-
-	Map<String, List<Link>> groupedItems ={};
+Map<String, List<Link>> group(items) {
+	Map<String, List<Link>> groupedItems = {};
 
 	var links = items.map(toLink);
 
@@ -67,11 +39,30 @@ Map<String, List<Link>> getLinks(items) {
 		groupedItems[groupName].add(link);
 	});
 
-	return reorder(groupedItems);
+	return groupedItems;
 }
 
-Map<String, List<Link>> reorder(Map<String, List<Link>> groupedItems) {
+Link toLink(PocketData item) {
+	var title = (item.resolvedTitle == null || item.resolvedTitle.isEmpty) ? item.givenTitle : item.resolvedTitle;
+	var url = item.resolvedUrl ?? item.givenUrl;
+	var tags = item.tags.map((Tag tag) => tag.tag).toList();
+	var imageUrl = item.hasImages ? item.images[0].sourceUrl : '';
 
+	return new Link(int.parse(item.itemId), title, item.excerpt, url, tags, imageUrl: imageUrl);
+}
+
+String getGroupName(List<String> tags) {
+	if (tags.contains('es6')) return 'ES6';
+	if (tags.contains('news')) return 'News';
+	if (tags.contains('javascript')) return 'JavaScript';
+	if (tags.contains('css')) return 'CSS';
+	if (tags.contains('.net')) return 'C#';
+	if (tags.contains('web')) return 'Web-development';
+	return 'Other';
+}
+
+
+Map<String, List<Link>> reorder(Map<String, List<Link>> groupedItems) {
 	var result = {};
 
 	if (groupedItems['JavaScript'] != null)
@@ -98,21 +89,45 @@ Map<String, List<Link>> reorder(Map<String, List<Link>> groupedItems) {
 	return result;
 }
 
-Link toLink(PocketData item) {
-	var title = (item.resolvedTitle == null || item.resolvedTitle.isEmpty) ? item.givenTitle : item.resolvedTitle;
-	var url = item.resolvedUrl ?? item.givenUrl;
-	var tags = item.tags.map((Tag tag) => tag.tag).toList();
-	var imageUrl = item.hasImages ? item.images[0].sourceUrl : '';
+Map<String, List<Link>> sendEmail(Map<String, List<Link>> links) {
+	var recipients = ['anechytailov@gmail.com'];
+	var title = 'Digest title';
 
-	return new Link(title, item.excerpt, url, tags, imageUrl: imageUrl);
+	var template =
+	'<h1>$title</h1>'
+	'<div>'
+	'<!-- start group repeat -->'
+	'<h2>{{Group}}:</h2>'
+	'<!-- start repeat -->'
+	'<div class="section">'
+	//	    '<img src={{ImageUrl}} style />'
+	'<div>'
+	'<a href=\'{{Url}}\'>{{Title}}</a>'
+	'<div>{{Description}}</div>'
+	'</div>'
+	'</div>'
+	'<!-- end repeat -->'
+	'<!-- end group repeat -->'
+	'</div>';
+
+	var generator = new MailGenerator(template, links);
+	var result = generator.generate();
+
+	var mailer = new Mailer();
+	mailer.send(title, result, recipients);
+
+	return links;
 }
 
-String getGroupName(List<String> tags){
-	if (tags.contains('es6')) return 'ES6';
-	if (tags.contains('news')) return 'News';
-	if (tags.contains('javascript')) return 'JavaScript';
-	if (tags.contains('css')) return 'CSS';
-	if (tags.contains('.net')) return 'C#';
-	if (tags.contains('web')) return 'Web-development';
-	return 'Other';
+cleanUp(Map<String, List<Link>> links) {
+	List<Action> actions = [];
+
+	links.values.forEach((group) {
+		group.forEach((link) {
+			actions.add(new RemoveTagsAction(link.id, ['buggy']));
+			actions.add(new ArchiveAction(link.id));
+		});
+	});
+
+	client.modify(actions);
 }
--- lib/src/LinkAggregator.dart
+++ lib/src/LinkAggregator.dart
@@ -1,20 +1,12 @@
 library EmailDigestGenerator.link_aggregator;
 
-class LinkAggregator {
-	static List<Link> getLinks() => [
-		new Link("First article", "The first one", "http://first", ["Digest","ES6"]),
-		new Link("Second article", "The second one", "http://second", ["News","Digest"]),
-		new Link("Last article", "The last one", "http://last", ["JavaScript"]),
-		new Link("Another article", "The anothe one", "http://other", [])
-	];
-}
-
 class Link {
+	int id;
 	String title;
 	String description;
 	String url;
 	List<String> tags;
 	String imageUrl;
 
-	Link(this.title, this.description, this.url, this.tags, {this.imageUrl});
+	Link(this.id, this.title, this.description, this.url, this.tags, {this.imageUrl});
 }
